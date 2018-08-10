aa1664b149cfca65be7e0cff9732425dfe5acd30
Nicole <nicole.sattler@live.com>
2018-08-10 10:08:13 -0500

    Move static assets code

--- server.js
+++ server.js
@@ -19,17 +19,17 @@ mongoose
 .then(() => console.log('Connected to DB'))
 .catch(err => console.log(err));
 
+// Use routes
+app.use('/api/items', items);
+
 // Serve static assets if in productions
-if(process.env.NODE_ENV == 'production') {
+if (process.env.NODE_ENV === 'production') {
    // Set static folder
    app.use(express.static('client/build'));
    app.get('*', (req, res) => {
       res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
    });
 }
-
-// Use routes
-app.use('/api/items', items);
 
 const port = process.env.PORT || 5000;
 
